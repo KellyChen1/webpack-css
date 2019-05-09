4d95d9e0e92ed6eb6cf734aac5b62b24b7d64973
chenlujia <862740899@qq.com>
2019-05-09 23:34:53 +0800

    img, font 	打包成功

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './index.css';
+import Icon from './assets/icon.png';
 let node = document.createElement("div");
-node.innerText="hi"
+node.innerText="hi, I am kelly"
+// 添加样式
 node.className="red"
+// 添加图片
+let img = new Image()
+img.src=Icon
+node.appendChild(img)
 document.getElementById("root").appendChild(node)       //在root里添加子元素，用appendChild
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,25 @@
-const path= require("path")
+const path = require("path");
 
-module.exports ={
-    entry:"./src/index.js",
-    output:{
-        path: path.resolve(__dirname,"dist"),
-        filename: "bundle.js",
-    },
-    module:{
-        rules:[
-            {
-                test:/\.css/,
-                use:[
-                    'style-loader',
-                    'css-loader'
-                ]
-            }
-        ]
-    }
-}
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)/,
+        use: ["file-loader"]
+      },
+      {
+          test:/\.(woff|woff2|eot|ttf|otf)$/,
+          use:["file-loader"]
+      }
+    ]
+  }
+};
