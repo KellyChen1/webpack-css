8695395dd42920fd99604ddc4a4ff33f81c5836e
chenlujia <862740899@qq.com>
2019-05-09 22:59:40 +0800

    css-放在了head style里面

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-window.onload=()=>{
-    alert("hi")
-}
+import './index.css';
+let node = document.createElement("div");
+node.innerText="hi"
+node.className="red"
+document.getElementById("root").appendChild(node)       //在root里添加子元素，用appendChild
